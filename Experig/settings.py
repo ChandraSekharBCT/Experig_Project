00e5e88f37b6f2c59e6d1a5378d8d3c2611cc6b6
ChandraSekharBCT <chandrasekharsahoo582@gmail.com>
2021-02-05 17:23:01 +0530

    dj static

--- Experig/settings.py
+++ Experig/settings.py
@@ -130,8 +130,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
-STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 STATICFILES_DIRS=[STATIC_DIR,]
 MEDIA_URL='/media/'
